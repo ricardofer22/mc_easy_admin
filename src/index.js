9665aaa07757bdb0bae7caa17419de502d60b70e
Ricardo <fernandezlorcaricardo@gmail.com>
2026-02-25 19:41:44 -0300

    child process exec on /run route

--- src/index.js
+++ src/index.js
@@ -1,4 +1,6 @@
 import express from 'express'
+import util from 'node:util'
+import child_process from 'node:child_process'
 
 const app = express()
 const port = 3000
@@ -8,6 +10,13 @@ app.set('views', 'src/views')
 
 app.get('/', (req, res) => {
   res.render('index')
+})
+
+const exec = util.promisify(child_process.exec)
+
+app.get('/run', async (req, res) => {
+  const { stdout, stderr } = await exec('ls -lh')
+  res.send(stdout)
 })
 
 app.listen(port, () => {
