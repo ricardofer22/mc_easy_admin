b5a0ad29fc72101fb844e85b1481c5ff44e7cef9
Ricardo <fernandezlorcaricardo@gmail.com>
2026-02-25 19:14:36 -0300

    init

---
+++ src/index.js
@@ -0,0 +1,15 @@
+import express from 'express'
+
+const app = express()
+const port = 3000
+
+app.set('view engine', 'ejs')
+app.set('views', 'src/views')
+
+app.get('/', (req, res) => {
+  res.render('index')
+})
+
+app.listen(port, () => {
+  console.log(`Server running at port ${port}`)
+})
